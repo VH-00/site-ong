4ef7abd73eb55eae867a684b18591f21ffef9c27
VH-00 <vitorhugolm86@gmail.com>
2025-06-20 11:39:32 -0300

    Update transparencia.js

--- js/transparencia.js
+++ js/transparencia.js
@@ -3,31 +3,31 @@ document.addEventListener('DOMContentLoaded', function() {
     const projetos = [
         {
             id: 1,
-            titulo: "Sede VAPI",
+            titulo: "Sede ",
             descricao: "Documentos financeiros da sede administrativa",
             imagem: "img/projetos/sede.jpg",
             link: "transparencia-sede.html"
         },
         {
             id: 2,
-            titulo: "Abrigo LGBTQIA+",
-            descricao: "Transparência do abrigo feminino",
-            imagem: "img/projetos/abrigo-lgbtqia.jpg",
-            link: "transparencia-abrigo.html"
+            titulo: "Projeto 1",
+            descricao: "Descrição 1",
+            imagem: "img/projetos/projeto1.jpg",
+            link: "projeto1.html"
         },
         {
             id: 3,
-            titulo: "Casa do Idoso",
-            descricao: "Prestação de contas do projeto",
-            imagem: "img/projetos/casa-idoso.jpg",
-            link: "transparencia-idoso.html"
+           titulo: "Projeto 2",
+            descricao: "Descrição 2",
+            imagem: "img/projetos/projeto2.jpg",
+            link: "projeto2.html"
         },
         {
             id: 4,
-            titulo: "Creches CEDINS",
-            descricao: "Recursos aplicados nas unidades educacionais",
-            imagem: "img/projetos/creches.jpg",
-            link: "transparencia-creches.html"
+            titulo: "Projeto 3",
+            descricao: "Descrição 3",
+            imagem: "img/projetos/projeto3.jpg",
+            link: "projeto3.html"
         }
     ];
 
